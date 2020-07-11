30dbdb0ecc739df7583fbd28de7a380c1be9ed47
david <david@appsaloon.be>
2020-07-12 00:40:29 +0200

    fix(expectConstructorNameToBe.js): fixed bug where Boolean "false" throws an error despite expecting a Boolean

--- src/lib/expectConstructorNameToBe.js
+++ src/lib/expectConstructorNameToBe.js
@@ -1,8 +1,12 @@
 const expectConstructorNameToBe = (value, constructorName, path, allowUndefined = false) => {
-  if (!value || !value.constructor || !value.constructor.name || value.constructor.name !== constructorName) {
-    if(!allowUndefined) {
+  try {
+    if (value.constructor.name !== constructorName) {
+      throw new Error()
+    }
+  } catch (error) {
+    if (!allowUndefined) {
       throw new Error(`Expected ${path} to be "${constructorName}"`)
-    } else if(typeof value !== 'undefined') {
+    } else if (typeof value !== 'undefined') {
       throw new Error(`Expected ${path} to be "undefined" or "${constructorName}"`)
     }
   }
